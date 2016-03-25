3a92d4c4385dffbf664f484c501880c3d59a96a2
Davit Petrosyan <davitp27@gmail.com>
2016-03-25 11:25:40 +0400

    add onTextChangeListener in MainActivity and add 11,12 lines on manifest

--- app/src/main/java/am/wedo/davit/tipcalculator/MainActivity.java
+++ app/src/main/java/am/wedo/davit/tipcalculator/MainActivity.java
@@ -2,6 +2,9 @@ package am.wedo.davit.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher; // EditText listener
+import android.widget.SeekBar.OnSeekBarChangeListener; // SeekBar listener
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -11,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     // currency and percent objects
     private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
-    private static final NumberFormat precentFormat = NumberFormat.getPercentInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
 
     private double billAmount = 0.0; // bill amount entered by the user
     private double percent = 0.15; // initial tip percentage
@@ -41,4 +44,61 @@ public class MainActivity extends AppCompatActivity {
         SeekBar percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
         percentSeekBar.setOnSeekBarChangeListener(seekBarListener);
     }
+
+    // calculate and display tip and total amounts
+    private void calculate() {
+         // format percent and display in percentTextView
+         percentTextView.setText(percentFormat.format(percent));
+
+         // calculate the tip and total
+         double tip = billAmount * percent;
+         double total = billAmount + tip;
+
+         // display tip and total formatted as currency
+         tipTextView.setText(currencyFormat.format(tip));
+         totalTextView.setText(currencyFormat.format(total));
+         }
+
+    // listener object for the SeekBar's progress changed events
+    private final OnSeekBarChangeListener seekBarListener = new OnSeekBarChangeListener() {
+        // update percent, then call calculate
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            percent = progress / 100.0; // set percent based on progress
+            calculate(); // calculate and display tip and total
+        }
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) { }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) { }
+    };
+
+    // listener object for the EditText's text-changed events
+    public final TextWatcher amountEditTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        // called when the user modifies the bill amount
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try { // get bill amount and display currency formatted value
+                    billAmount = Double.parseDouble(s.toString()) / 100.0;
+                    amountTextView.setText(currencyFormat.format(billAmount));
+                }
+            catch (NumberFormatException e) { // if s is empty or non-numeric
+                amountTextView.setText("");
+                billAmount = 0.0;
+            }
+
+            calculate(); // update the tip and total TextViews
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
